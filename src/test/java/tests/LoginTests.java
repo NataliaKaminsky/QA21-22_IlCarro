7628bcc1532b7adac977a27d4456006a54e4fe5a
NataliaKaminsky <124137357+NataliaKaminsky@users.noreply.github.com>
2023-12-15 10:01:02 +0200

    pattern

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,10 @@
+package tests;
+
+import manager.ApplicationManager;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class LoginTests extends TestBase {
+
+
+}
